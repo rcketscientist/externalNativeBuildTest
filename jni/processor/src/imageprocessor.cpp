28831f72870506d501845a3c68a5daccc5dce573
Anthony <amandra@gmail.com>
2016-06-14 22:19:05 +0200

    add stdio.h to imageprocessor, it's actually documented that the jpeglib.h caller needs to include it, the project this was pulled from had it included deep in another header

--- jni/processor/src/imageprocessor.cpp
+++ jni/processor/src/imageprocessor.cpp
@@ -7,6 +7,7 @@
 #define  LOGI(...)  ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 #define  LOGE(...)  ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
 
+#include <stdio.h> //required by jpeglib.h
 #include <jpeglib.h>
 #include <assert.h>
 
